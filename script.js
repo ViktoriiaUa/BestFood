dede3e2b6c945ca1881817870bcbbf277f86e922
ViktoriiaUa <andriivna_v@ukr.net>
2023-07-05 18:10:45 +0300

    first5 commit

---
+++ script.js
@@ -0,0 +1,82 @@
+// ....Menu
+const divtoShow = 'nav .menu';
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector('.m-trigger');
+
+divTrigger.addEventListener('click', () => {
+  setTimeout(() => {
+    if (!divPopup.classList.contains('show')) {
+      divPopup.classList.add('show');
+      document.body.classList.add('menu-visible')
+    }
+  }, 250);
+})
+
+
+// automatically close by clock outside menu
+document.addEventListener('click', (e) => {
+  const isClosest = e.target.closest(divtoShow);
+
+  if (!isClosest && divPopup.classList.contains('show')) {
+    divPopup.classList.remove('show');
+    document.body.classList.remove('menu-visible')
+  }
+})
+
+
+// ...Search botn
+const sTrigger = document.querySelector('.s-trigger');
+const addclass = document.querySelector('.site');
+sTrigger.addEventListener('click', () => {
+  addclass.classList.toggle('showsearch')
+})
+
+
+
+
+
+// Slider
+const sliderThumb = new Swiper('.thumb-nav', {
+  spaceBetween: 10,
+  slidePerView: 3,
+  slidesPerGroup: false,
+  breakpoints: {
+    992: {
+      direction: 'vertical'
+    }
+  }
+});
+
+
+const theSlider = new Swiper('.thumb-big', {
+  slidePerView: 1,
+  pagination: {
+      el: '.swiper-pagination',
+  },
+  thumbs: {
+    swiper: sliderThumb,
+  }
+});
+
+
+
+
+
+// /.....on scroll transition
+const io = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add('this')
+    }
+  })
+})
+
+
+const box = document.querySelectorAll('.animate');
+    box.forEach((el) => {
+      io.observe(el);
+    })
+
+
+
+    
